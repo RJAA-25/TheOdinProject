dcf9108be8c1265e11a0d60ee8b2b36d3465fac0
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-02-09 23:23:40 +0800

    Added solution to Caesar Cipher project

---
+++ Ruby/CaesarCipher/caesar_cipher_solution.rb
@@ -0,0 +1,18 @@
+def caesar_cipher(message, shift = 0)
+    store = message.split("")
+    base_upper = 65
+    base_lower = 97
+    encrypt = store.map do |letter|
+        shifted = letter.ord + shift.to_i
+        if letter.ord.between?(65,90)
+            ((shifted - base_upper) % 26 + base_upper).chr 
+        elsif letter.ord.between?(97,122)
+            ((shifted - base_lower) % 26 + base_lower).chr
+        else 
+            letter
+        end
+    end
+    encrypt.join("")
+end 
+
+puts caesar_cipher("What a string!", 5.3)
